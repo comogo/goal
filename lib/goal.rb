1fd21bd3ec777ecb76baceba878e77d3ca669b44
Mateus Lorandi dos Santos <mcomogo@gmail.com>
2013-10-21 21:58:02 -0200

    Remove Base and add Calculator
    Calculator now generate data to the report.
    
    This simplify the bin file.

--- lib/goal.rb
+++ lib/goal.rb
@@ -1,6 +1,69 @@
 require 'goal/hour_calculator'
 require 'goal/freshbooks_calculator'
-require 'goal/base'
+require 'goal/report'
 
 module Goal
+  class Calculator
+    def initialize(options = {})
+      @calculator = Goal::HourCalculator.new
+      @goal_list = options.fetch(:goal_list, [160, 200, 250, 300])
+      @username  = options[:username]
+      @token     = options[:token]
+      @hours     = options[:hours]
+    end
+
+    def worked_time
+      if hours
+        hours
+      elsif username && token
+        freshbooks.hours
+      else
+        0
+      end.round(2)
+    end
+
+    # It calculates the current hour rate
+    def rate
+      calculator.hour_rate(worked_time).round(2)
+    end
+
+    def generate_data
+      data = {}
+
+      data[:rows] = goals
+      data[:summary] = summary
+
+      data
+    end
+
+    private
+
+    attr_reader :base, :calculator, :goal_list, :username, :token, :hours
+
+    def freshbooks
+      Goal::FreshbooksCalculator.new(username, token)
+    end
+
+    def goals
+      calculated_goals = []
+
+      goal_list.each do |goal|
+        calculated_goals.push({
+          goal: goal,
+          expected: calculator.estimated_for(goal).round(2),
+          average: calculator.rate_to_goal(goal, worked_time).round(2)
+        })
+      end
+
+      calculated_goals
+    end
+
+    def summary
+      {
+        total_time: worked_time,
+        rate: rate,
+        days_left: calculator.days_left
+      }
+    end
+  end
 end
---
+++ spec/calculator_spec.rb
@@ -0,0 +1,72 @@
+require 'spec_helper'
+
+describe Goal::Calculator do
+  describe '#worked_time' do
+    context 'with hours' do
+      subject do
+        described_class.new(hours: 90)
+      end
+
+      it 'should return hours' do
+        expect(subject.worked_time).to eq 90
+      end
+    end
+
+    context 'with hours' do
+      subject do
+        described_class.new(username: 'username', token: 'token')
+      end
+
+      it 'should return the freshbooks hours' do
+        instance = double(:instance)
+
+        Goal::FreshbooksCalculator.should_receive(:new).with('username', 'token').and_return(instance)
+        instance.should_receive(:hours).and_return 55.55
+
+        expect(subject.worked_time).to eq 55.55
+      end
+    end
+  end
+
+  describe '#rate' do
+    subject do
+      described_class.new(hours: 90)
+    end
+
+    it 'should return calculate the rate' do
+      Date.stub(today: Date.new(2013, 10, 20))
+
+      expect(subject.rate).to eq 6.43
+    end
+  end
+
+  describe '#generate_data' do
+    subject do
+      described_class.new(hours: 90, goal_list: [160, 200])
+    end
+
+    it 'should return the generated data' do
+      Date.stub(today: Date.new(2013, 10, 20))
+
+      expect(subject.generate_data).to eq({
+        rows: [
+          {
+            goal: 160,
+            expected: 97.39,
+            average: 7.78
+          },
+          {
+            goal: 200,
+            expected: 121.74,
+            average: 12.22
+          }
+        ],
+        summary: {
+          total_time: 90.0,
+          rate: 6.43,
+          days_left: 9
+        }
+      })
+    end
+  end
+end
